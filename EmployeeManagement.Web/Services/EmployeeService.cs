7c305325174c10dfef59f5d214d7c7cb4ec2e651
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-10-10 22:21:20 +0630

    Start-Call REST API from Blazor
    Add services folder,create interface and class of employee service, add HttpClient service in startup.cs and download Microsoft.AspNetCore.Blazor.HttpClient nuget package

---
+++ EmployeeManagement.Web/Services/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public Task<IEnumerable<Employee>> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
