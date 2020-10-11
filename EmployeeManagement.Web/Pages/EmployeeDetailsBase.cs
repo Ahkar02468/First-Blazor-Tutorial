4b32f5b3608ead2b8638596c64fa55f7bb9da2d4
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-10-11 12:42:30 +0630

    Start-Balazor Route Parameter
    EmployeeDetails inherits from EmployeeDetailsbase. Declare new method in EmplyeeService to route to EmployeDetails.

---
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        [Parameter]
+        public int ID { get; set; }
+    }
+}
