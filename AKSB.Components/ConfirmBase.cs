e8c56165065d772311cc74da50e851606db6671c
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-10-26 22:32:12 +0630

    Show Delete COnfirmation - Start
    Create bootstrap modal to show up.

---
+++ AKSB.Components/ConfirmBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AKSB.Components
+{
+    public class ConfirmBase : ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; }
+        public void show()
+        {
+            ShowConfirmation = true;
+        }
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+    }
+}
